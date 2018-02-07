6f2c5f647e9ef6b68a6653e56fc184e5edc7ad6b
Juliana Tamizaki Aquino <a00995303@se14l0124-22.ad.bcit.ca>
2018-02-06 16:53:18 -0800

    socket

---
+++ index.js
@@ -0,0 +1,35 @@
+const port = process.env.PORT || 10000;
+const server = require("http").Server();
+
+var io = require("socket.io")(server);
+
+var allusers = [];
+
+io.on("connection", function(socket){
+    console.log("connect");
+    allusers.push(socket.id);
+    console.log(allusers);
+    
+    socket.emit("yourid", socket.id);
+    
+    io.emit("userjoined", allusers);
+    
+    socket.on("mymove", function(data){
+        //broadcast = send everybody but myself    
+       socket.broadcast.emit("newmove", data);       
+    });
+    
+    socket.on("disconnect", function(){
+        var index = allusers.indexOf(socket.id);
+        allusers.splice(index, 1);
+        io.emit("userjoined", allusers);
+    })
+});
+
+server.listen(port, (err)=>{
+              if(err){
+    console.log(err);
+    return false;
+}
+console.log("Port is running");
+})
